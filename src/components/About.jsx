83e657cbe74af05f67fe76133e2966c47e094080
rakshitak <rakshita.kotian@vistaura.com>
2023-07-31 11:24:45 +0530

    Resolved dependencies

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in  
+        I'm a skilled software developer with experience in
         JavaScript, and expertise in frameworks like React and
         Three.js. I'm a quick learner.
         Let's work together to bring your ideas to life!
